727db408ede9b4766053afabd8fdbd68c9b1db0b
Tim B. <werpuzzlemoon@gmail.com>
2014-09-10 16:56:07 -0400

    Added lib as it stands.
    History of the library prior to this repo can be found in my "BaseLibs"
    repository.

---
+++ tile.lua
@@ -0,0 +1,107 @@
+local tile = {}
+
+local micro = .001
+local tileSize
+
+--Returns positive 1 for positive numbers, -1 for negative numbers and 0 for 0.
+local function sign(x)
+	return x < 0 and -1 or (x > 0 and 1 or 0)
+end
+
+--Slightly faster version of math.min()
+local function min(a,b) return a < b and a or b end
+
+--Decides how to order the parameters passed to tile.checkPoint to supposedly reduce
+--complications. If axis is true, the x axis is being checked, and y otherwise.
+local function checkPointAxis(x, y, axis)
+	if axis then ans = tile.checkPoint(x, y)
+	else ans = tile.checkPoint(y, x) end
+	return ans
+end
+
+--A shortcut to point checking that can check three coordinates at a time.
+local function checkPoints(edge, t, axis)
+	if checkPointAxis(t[1], edge, axis) or
+	checkPointAxis(t[2], edge, axis) or
+	checkPointAxis(t[3], edge, axis) then
+	return true end
+end
+
+--Collides either axis with the tiles and returns the axis's
+--post-collision coordinate, velocity and normal.
+local function collideAxis(a,b,l1,l2,v,vs,axis)
+	local check = true
+	local n = 0
+	
+	v = v*vs
+	local fmin, fplus = a + v, a + l1 + v
+	check = true
+	if vs == 1 then edge = fplus divi = fplus
+	elseif vs == -1 then edge = fmin divi = a
+	else check = false end
+	if check and checkPoints(edge, {b, b+l2-micro, b+l2/2}, axis) then
+		a = math.floor(divi / tileSize) * tileSize
+		if vs == 1 then a = a - l1 end
+		v = 0
+		n = -vs
+	end
+	a = a + v
+	return a, v, n
+end
+
+--Ensures that colliding objects cannot go through tiles if they are moving very fast.
+local function collideNoTunnel(a,b,l1,l2,v,axis)
+	local vsign, av, maxv
+	maxv = tileSize-micro
+	vsign = sign(v)
+	av = math.abs(v)
+	if av < tileSize then
+		a, v, n = collideAxis(a,b,l1,l2,av,vsign,axis)
+	else
+		while av > 0 do
+			local nowv = min(maxv, av)
+			a, nowv, n = collideAxis(a,b,l1,l2,nowv,vsign,axis)
+			if n ~= 0 then
+				v = nowv
+				break
+			end
+			av = av - maxv
+		end
+	end
+	return a, v, n
+end
+
+--Public interface:
+
+--Sets the size of each tile in the grid.
+function tile.setSize(x) tileSize = x return tileSize end
+
+--Gets the size of each tile in the grid.
+function tile.getSize() return tileSize end
+
+--Takes a table of rectangle coors, dimensions and velocities. Returns a
+--table with post-collision coors, velocities and normals for each axis.
+function tile.collide(rect)
+	local x, y = rect.x, rect.y
+	local nx, ny = 0
+	local w, h = rect.w, rect.h
+	local vx, vy = rect.vx, rect.vy
+	
+	x, vx, nx = collideNoTunnel(x,y,w,h,vx,false)
+	
+	y, vy, ny = collideNoTunnel(y,x,h,w,vy,true)
+	
+	return {x = x, y = y, vx = vx, vy = vy, nx = nx, ny = ny}
+end
+
+--Converts pixel coordinates to grid coordinates and passes them to checkGrid.
+function tile.checkPoint(x, y)
+	return tile.checkGrid(math.floor(y/tileSize), math.floor(x/tileSize))
+end
+
+--OVERRIDABLE. Returns true if the given point on the tile grid is solid,
+--i.e. should be collided with.
+function tile.checkGrid(x, y)
+end
+
+return tile
